a16a2a137eefae9a4ed2807c05e311a17e7c82ca
ky-s <kyohei_86@hotmail.com>
2019-11-02 23:28:21 +0900

    [update] add String#to_regexp,to_re

--- lib/ky_s_extensions/string.rb
+++ lib/ky_s_extensions/string.rb
@@ -1,3 +1,9 @@
 class String
   alias_method :to_a, :split
+
+  def to_regexp
+    /#{self}/
+  end
+
+  alias_method :to_re, :to_regexp
 end
--- test/string_test.rb
+++ test/string_test.rb
@@ -6,4 +6,9 @@ class StringTest < Minitest::Test
     assert_equal '1,2,3'.split(','), '1,2,3'.to_a(',')
     assert_equal '1,2,3'.split(',', 2), '1,2,3'.to_a(',', 2)
   end
+
+  def test_to_regexp
+    assert_equal /^[0-9A-Za-z]+$/, '^[0-9A-Za-z]+$'.to_regexp
+    assert_equal /^[0-9A-Za-z]+$/, '^[0-9A-Za-z]+$'.to_re
+  end
 end
